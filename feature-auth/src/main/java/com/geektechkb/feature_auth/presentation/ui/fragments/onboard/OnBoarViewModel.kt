32d6fa332ef911cb97d4bd825ae8442892140599
user <adiletu40@gmail.com>
2022-05-31 11:59:51 +0600

    added onBoardViewModel

---
+++ feature-auth/src/main/java/com/geektechkb/feature_auth/presentation/ui/fragments/onboard/OnBoarViewModel.kt
@@ -0,0 +1,10 @@
+package com.geektechkb.feature_auth.presentation.ui.fragments.onboard
+
+import com.geektechkb.core.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+@HiltViewModel
+class OnBoarViewModel @Inject constructor(
+
+) : BaseViewModel() {
+}
