af3fb988e096685b61bd851c385a49543235d7b7
Clar1fy <timplifier@gmail.com>
2022-06-02 08:55:11 +0600

    migrating from extensions to append text to the edittext to methods

--- feature-auth/src/main/java/com/geektechkb/feature_auth/presentation/ui/fragments/auth/signUp/SignUpFragment.kt
+++ feature-auth/src/main/java/com/geektechkb/feature_auth/presentation/ui/fragments/auth/signUp/SignUpFragment.kt
@@ -1,13 +1,11 @@
 package com.geektechkb.feature_auth.presentation.ui.fragments.auth.signUp
 
-import android.widget.TextView
 import androidx.hilt.navigation.fragment.hiltNavGraphViewModels
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.geektechkb.core.base.BaseFragment
 import com.geektechkb.feature_auth.R
 import com.geektechkb.feature_auth.databinding.FragmentSignUpBinding
-import com.github.pinball83.maskededittext.MaskedEditText
 import com.google.android.material.textfield.TextInputEditText
 import com.timplifier.firebaseauthenticationtest.presentation.extensions.directionsSafeNavigation
 import dagger.hilt.android.AndroidEntryPoint
@@ -56,16 +54,46 @@ class SignUpFragment :
 
     private fun setupNumericKeyboardListener() {
         binding.apply {
-            tvOne.setOnNumericClickListener(etPhone)
-            tvTwo.setOnNumericClickListener(etPhone)
-            tvThree.setOnNumericClickListener(etPhone)
-            tvFour.setOnNumericClickListener(etPhone)
-            tvFive.setOnNumericClickListener(etPhone)
-            tvSix.setOnNumericClickListener(etPhone)
-            tvSeven.setOnNumericClickListener(etPhone)
-            tvEight.setOnNumericClickListener(etPhone)
-            tvNine.setOnNumericClickListener(etPhone)
-            tvZero.setOnNumericClickListener(etPhone)
+            tvOne.setOnClickListener {
+                setOnNumericClickListener(binding.etPhone,tvOne.text)
+            }
+            tvTwo.setOnClickListener {
+                setOnNumericClickListener(binding.etPhone,tvTwo.text)
+            }
+            tvThree.setOnClickListener {
+                setOnNumericClickListener(binding.etPhone,tvThree.text)
+            }
+            tvFour.setOnClickListener {
+                setOnNumericClickListener(binding.etPhone,tvFour.text)
+            }
+            tvFive.setOnClickListener {
+                setOnNumericClickListener(binding.etPhone,tvFive.text)
+            }
+            tvSix.setOnClickListener {
+                setOnNumericClickListener(binding.etPhone,tvSix.text)
+            }
+            tvSeven.setOnClickListener {
+                setOnNumericClickListener(binding.etPhone,tvSeven.text)
+            }
+            tvEight.setOnClickListener {
+                setOnNumericClickListener(binding.etPhone,tvEight.text)
+            }
+            tvNine.setOnClickListener {
+                setOnNumericClickListener(binding.etPhone,tvNine.text)
+            }
+            tvZero.setOnClickListener {
+                setOnNumericClickListener(binding.etPhone,tvZero.text)
+            }
+//            tvOne.setOnNumericClickListener(etPhone)
+//            tvTwo.setOnNumericClickListener(etPhone)
+//            tvThree.setOnNumericClickListener(etPhone)
+//            tvFour.setOnNumericClickListener(etPhone)
+//            tvFive.setOnNumericClickListener(etPhone)
+//            tvSix.setOnNumericClickListener(etPhone)
+//            tvSeven.setOnNumericClickListener(etPhone)
+//            tvEight.setOnNumericClickListener(etPhone)
+//            tvNine.setOnNumericClickListener(etPhone)
+//            tvZero.setOnNumericClickListener(etPhone)
         }
     }
 
@@ -76,15 +104,9 @@ class SignUpFragment :
 
     }
 
-    private fun deleteASingleCharacterInEditTextPressingBackspace(editText: MaskedEditText) {
-        val cursorPosition = editText.selectionStart
-        if (cursorPosition > 0)
-            editText.text?.delete(cursorPosition - 1, cursorPosition)
-
-
+    private fun setOnNumericClickListener(editText: TextInputEditText, text: CharSequence) {
+        editText.text?.append(text)
     }
-}
 
-fun TextView.setOnNumericClickListener(editText: MaskedEditText) {
-    editText.text?.append(text)
+
 }
