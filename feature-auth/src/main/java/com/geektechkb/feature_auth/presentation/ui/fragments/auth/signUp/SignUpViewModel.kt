bac84be1f85b8ca4e7a92372edd65b841d35795f
Clar1fy <timplifier@gmail.com>
2022-06-06 11:23:06 +0600

    feat: ready-to-go signUpViewModel

--- feature-auth/src/main/java/com/geektechkb/feature_auth/presentation/ui/fragments/auth/signUp/SignUpViewModel.kt
+++ feature-auth/src/main/java/com/geektechkb/feature_auth/presentation/ui/fragments/auth/signUp/SignUpViewModel.kt
@@ -1,42 +1,54 @@
 package com.geektechkb.feature_auth.presentation.ui.fragments.auth.signUp
 
-import android.content.Context
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.geektechkb.core.base.BaseViewModel
-import com.geektechkb.feature_auth.data.repositories.authentication.AuthRepositoryImpl
+import com.geektechkb.feature_auth.domain.typealiases.NotAnActualActivity
+import com.geektechkb.feature_auth.domain.typealiases.NotAnActualCallbacks
+import com.geektechkb.feature_auth.domain.typealiases.NotAnActualFirebaseAuth
+import com.geektechkb.feature_auth.domain.useCases.authentication.IsUserAuthenticatedUseCase
+import com.geektechkb.feature_auth.domain.useCases.authentication.ProvideAuthenticationCallbacksUseCase
+import com.geektechkb.feature_auth.domain.useCases.authentication.StartPhoneNumberVerificationUseCase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthProvider
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class SignUpViewModel @Inject constructor(
     val firebaseAuth: FirebaseAuth,
-    private val authRepositoryImpl: AuthRepositoryImpl,
     private val isUserAuthenticatedUseCase: IsUserAuthenticatedUseCase,
+    private val provideAuthenticationCallbacksUseCase: ProvideAuthenticationCallbacksUseCase,
+    private val startPhoneNumberVerificationUseCase: StartPhoneNumberVerificationUseCase
 ) : BaseViewModel() {
-    fun setRussianLanguageCode() {
-        firebaseAuth.setLanguageCode("ru")
-    }
+
 
     fun signUp() = isUserAuthenticatedUseCase()
-    fun provideCallback(
-        context: Context
-    ) = authRepositoryImpl.provideAuthCallback(
-        authenticationSucceed = {
-            Toast.makeText(context, "You have requested verification code", Toast.LENGTH_SHORT)
-                .show()
-        },
-        authInvalidCredentialsError = {
-            Toast.makeText(context, "You have entered the wrong number", Toast.LENGTH_SHORT).show()
+    fun provideCallbacks(
+        authenticationSucceeded: ((() -> Unit))?,
+        authInvalidCredentialsError: ((() -> Unit))?,
+        tooManyRequestsError: ((() -> Unit))?
+    ) = provideAuthenticationCallbacksUseCase(authenticationSucceeded = {
+        authenticationSucceeded?.invoke()
+    }, authInvalidCredentialsError = {
+        authInvalidCredentialsError?.invoke()
+    }, tooManyRequestsError = {
+        tooManyRequestsError?.invoke()
+    }
 
-        },
-        tooManyRequestsError = {
+    ) as PhoneAuthProvider.OnVerificationStateChangedCallbacks
 
-            Toast.makeText(
-                context,
-                "You have exceeded verification code request limit",
-                Toast.LENGTH_SHORT
-            ).show()
-        }
-    )
+    fun startPhoneNumberVerification(
+        notAnActualFirebaseAuth: NotAnActualFirebaseAuth,
+        phoneNumber: String,
+        notAnActualActivity: NotAnActualActivity,
+        notAnActualCallbacks: NotAnActualCallbacks,
+    ) {
+        startPhoneNumberVerificationUseCase(
+            notAnActualFirebaseAuth as FirebaseAuth,
+            phoneNumber,
+            notAnActualActivity as AppCompatActivity,
+            notAnActualCallbacks as PhoneAuthProvider.OnVerificationStateChangedCallbacks
+        )
+        notAnActualFirebaseAuth.setLanguageCode("ru-RU")
+    }
 }
