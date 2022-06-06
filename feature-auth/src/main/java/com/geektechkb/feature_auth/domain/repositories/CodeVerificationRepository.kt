16656650cba71000f732fad8854fa10dc7c3073f
Clar1fy <timplifier@gmail.com>
2022-06-06 11:13:40 +0600

    feat: rewrite codeverification repository

--- feature-auth/src/main/java/com/geektechkb/feature_auth/domain/repositories/CodeVerificationRepository.kt
+++ feature-auth/src/main/java/com/geektechkb/feature_auth/domain/repositories/CodeVerificationRepository.kt
@@ -4,7 +4,7 @@ import com.geektechkb.feature_auth.domain.typealiases.*
 
 interface CodeVerificationRepository {
 
-    fun getVerificationId(): String
+    fun getVerificationId(): String?
     fun verifyPhoneNumberWithCode(
         verificationId: String,
         code: String,
