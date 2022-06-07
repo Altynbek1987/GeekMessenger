82a03c07115e615d2591a29615e5aa8eff327ee1
muradst <krasnodarceboksary@gmail.com>
2022-06-07 17:02:26 +0600

    default change

--- feature-main/src/main/java/com/geektechkb/feature_main/presentation/ui/fragments/group/GroupFragment.kt
+++ feature-main/src/main/java/com/geektechkb/feature_main/presentation/ui/fragments/group/GroupFragment.kt
@@ -1,8 +1,11 @@
 package com.geektechkb.feature_main.presentation.ui.fragments.group
 
+import android.util.Log
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.geektechkb.core.base.BaseFragment
+import com.geektechkb.core.utils.CheckInternet
 import com.geektechkb.feature_main.R
 import com.geektechkb.feature_main.databinding.FragmentGroupBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -10,9 +13,12 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class GroupFragment : BaseFragment<FragmentGroupBinding, GroupViewModel>(
     R.layout.fragment_group
+
 ) {
+
     override val binding by viewBinding(FragmentGroupBinding::bind)
     override val viewModel by viewModels<GroupViewModel>()
     override fun assembleViews() {
     }
+
 }
