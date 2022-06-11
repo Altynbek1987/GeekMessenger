4543062d76b76c72159b97e2fcfa25e700c007bf
user <adiletu40@gmail.com>
2022-06-11 19:10:50 +0600

    fixed bug

--- feature-auth/src/main/java/com/geektechkb/feature_auth/presentation/ui/fragments/onboard/OnBoardFragment.kt
+++ feature-auth/src/main/java/com/geektechkb/feature_auth/presentation/ui/fragments/onboard/OnBoardFragment.kt
@@ -1,8 +1,8 @@
 package com.geektechkb.feature_auth.presentation.ui.fragments.onboard
 
-import android.os.Bundle
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.viewpager2.widget.ViewPager2
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.geektechkb.core.base.BaseFragment
 import com.geektechkb.feature_auth.R
@@ -18,7 +18,6 @@ class OnBoardFragment :
     BaseFragment<FragmentOnBoardBinding, OnBoarViewModel>(R.layout.fragment_on_board) {
     override val binding by viewBinding(FragmentOnBoardBinding::bind)
     override val viewModel: OnBoarViewModel by viewModels()
-    private var currentPagerItemState: Int? = null
 
     private val viewPagerAdapter = ViewPagerAdapter(this::onItemClick)
 
@@ -30,10 +29,26 @@ class OnBoardFragment :
         binding.pager.setOnClickListener {
             binding.pager.setCurrentItem(binding.pager.currentItem + 1, true)
         }
+        binding.startBtn.setOnClickListener {
+            binding.pager.setCurrentItem(binding.pager.currentItem + 1, true)
+        }
+
     }
 
     override fun assembleViews() {
         setupAdapter()
+        changeButtonTextDependingOnPagerCurrentItem()
+    }
+
+    private fun changeButtonTextDependingOnPagerCurrentItem() {
+        binding.pager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                when (position) {
+                    2 -> binding.startBtn.text = "Погнали!"
+                    else -> binding.startBtn.text = "Далее"
+                }
+            }
+        })
     }
 
     private fun setupAdapter() {
@@ -49,22 +64,19 @@ class OnBoardFragment :
             BoardModel(
                 R.drawable.ic_group_24,
                 "GeekMessage",
-                "Добро пожаловать в GeekMessage",
-                "NEXT"
+                "Добро пожаловать в GeekMessenger",
             )
         )
         list.add(
             BoardModel(
-                R.drawable.communicationpng, "Описание", "Отличный и удобный messanger",
-                "NEXT"
+                R.drawable.communicationpng, "Описание", "Отличный и удобный messenger",
             )
         )
         list.add(
             BoardModel(
-                R.drawable.ideapng ,
+                R.drawable.ideapng,
                 " Погнали",
                 "Скорее нажимай на кнопку",
-                "START"
             )
         )
 
@@ -75,16 +87,14 @@ class OnBoardFragment :
         when (position) {
             0 -> {
                 binding.pager.setCurrentItem(1, true)
-                currentPagerItemState = 1
             }
             1 -> {
                 binding.pager.setCurrentItem(2, true)
-                currentPagerItemState = 2
             }
             2 -> {
+
                 preferences.hasOnBoardBeenShown = true
                 findNavController().navigate(R.id.action_onBoardFragment_authWithPhoneNumber)
-                currentPagerItemState = 3
             }
 
         }
--- feature-auth/src/main/java/com/geektechkb/feature_auth/presentation/ui/model/BoardModel.kt
+++ feature-auth/src/main/java/com/geektechkb/feature_auth/presentation/ui/model/BoardModel.kt
@@ -4,5 +4,4 @@ data class BoardModel(
     val image: Int,
     val title: String,
     val description: String,
-    val bottomName : String
 )
