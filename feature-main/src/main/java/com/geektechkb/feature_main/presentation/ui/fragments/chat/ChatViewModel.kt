8012364704a407eb0c866d3c91dbac61160e9f58
Clar1fy <timplifier@gmail.com>
2022-06-11 18:46:38 +0600

    fix: add authentication when creating profile

--- feature-main/src/main/java/com/geektechkb/feature_main/presentation/ui/fragments/chat/ChatViewModel.kt
+++ feature-main/src/main/java/com/geektechkb/feature_main/presentation/ui/fragments/chat/ChatViewModel.kt
@@ -2,7 +2,8 @@ package com.geektechkb.feature_main.presentation.ui.fragments.chat
 
 import com.geektechkb.core.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class ChatViewModel : BaseViewModel() {
+class ChatViewModel @Inject constructor() : BaseViewModel() {
 }
