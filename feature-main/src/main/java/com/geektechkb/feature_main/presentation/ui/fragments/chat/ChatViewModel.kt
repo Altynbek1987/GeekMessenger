4f2483e2259d0ce2a9132515ee47fa3044f3c31c
Clar1fy <timplifier@gmail.com>
2022-06-11 18:14:24 +0600

    feat(chatFragment-layout): add toolbar with appbar as a container; add menu for toolbar with icons

---
+++ feature-main/src/main/java/com/geektechkb/feature_main/presentation/ui/fragments/chat/ChatViewModel.kt
@@ -0,0 +1,8 @@
+package com.geektechkb.feature_main.presentation.ui.fragments.chat
+
+import com.geektechkb.core.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class ChatViewModel : BaseViewModel() {
+}
