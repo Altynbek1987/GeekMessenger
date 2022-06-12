0890ac34aa93864383d83a8753763a8e0546ba07
Clar1fy <timplifier@gmail.com>
2022-06-12 14:32:33 +0600

    feat: finally could add overflow menu for toolbar with icons, before they would not be visible; almost finish design

--- feature-main/src/main/java/com/geektechkb/feature_main/presentation/ui/fragments/chat/ChatFragment.kt
+++ feature-main/src/main/java/com/geektechkb/feature_main/presentation/ui/fragments/chat/ChatFragment.kt
@@ -1,7 +1,5 @@
 package com.geektechkb.feature_main.presentation.ui.fragments.chat
 
-import android.os.Bundle
-import android.view.*
 import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.geektechkb.core.base.BaseFragment
@@ -13,25 +11,6 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class ChatFragment : BaseFragment<FragmentChatBinding, ChatViewModel>(R.layout.fragment_chat) {
     override val binding by viewBinding(FragmentChatBinding::bind)
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        setHasOptionsMenu(true)
-        return super.onCreateView(inflater, container, savedInstanceState)
-    }
-
-    override fun assembleViews() {
-        activity?.actionBar?.setDisplayHomeAsUpEnabled(true)
-    }
-
-
     override val viewModel: ChatViewModel by viewModels()
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.chat_toolbar_menu, menu)
-        super.onCreateOptionsMenu(menu, inflater)
-    }
-
 
 }
