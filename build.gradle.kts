f8763de38de7e3d2bc2b18c335c8005157bb2b6a
Clar1fy <timplifier@gmail.com>
2022-07-16 16:41:51 +0600

    fix(build.gradle.kts-(:app)): migrate to 11 java version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.agp.application) apply false
     alias(libs.plugins.agp.library) apply false
