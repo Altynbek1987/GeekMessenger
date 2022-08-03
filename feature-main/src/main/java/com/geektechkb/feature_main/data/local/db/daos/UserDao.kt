b8da911eb21cce81f817e3b68108c0caca167a01
vavKLC <batyrkanov007@gmail.com>
2022-08-04 01:15:16 +0600

    добавил UserDao

---
+++ feature-main/src/main/java/com/geektechkb/feature_main/data/local/db/daos/UserDao.kt
@@ -0,0 +1,20 @@
+package com.geektechkb.feature_main.data.local.db.daos
+
+import androidx.room.*
+import com.geektechkb.feature_main.domain.models.User
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface UserDao {
+    @Query("SELECT * FROM user")
+    fun getUsers(): Flow<List<User>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertUser(user: User)
+
+    @Delete
+    suspend fun deleteUsers(user: User)
+
+    @Update
+    suspend fun updateUsers(user: User)
+}
