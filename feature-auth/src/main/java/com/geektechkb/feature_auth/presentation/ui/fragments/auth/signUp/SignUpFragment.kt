72517451a1eea5e91657d5a4f280933da3803d6f
Clar1fy <timplifier@gmail.com>
2022-08-06 02:25:12 +0600

    initialized mask for edittext to separate every 3 digits by a space

--- feature-auth/src/main/java/com/geektechkb/feature_auth/presentation/ui/fragments/auth/signUp/SignUpFragment.kt
+++ feature-auth/src/main/java/com/geektechkb/feature_auth/presentation/ui/fragments/auth/signUp/SignUpFragment.kt
@@ -4,20 +4,39 @@ import androidx.hilt.navigation.fragment.hiltNavGraphViewModels
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.geektechkb.core.base.BaseFragment
+import com.geektechkb.core.extensions.addTextChangedListenerAnonymously
 import com.geektechkb.core.extensions.directionsSafeNavigation
 import com.geektechkb.feature_auth.R
+import com.geektechkb.feature_auth.data.local.preferences.AuthorizePreferences
 import com.geektechkb.feature_auth.databinding.FragmentSignUpBinding
-import com.santalu.maskara.MaskChangedListener
+import com.vicmikhailau.maskededittext.MaskedFormatter
+import com.vicmikhailau.maskededittext.MaskedWatcher
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 
 @AndroidEntryPoint
 class SignUpFragment :
     BaseFragment<FragmentSignUpBinding, SignUpViewModel>(R.layout.fragment_sign_up) {
+    @Inject
+    lateinit var authorizePreferences: AuthorizePreferences
+
     override val binding by viewBinding(FragmentSignUpBinding::bind)
     override val viewModel: SignUpViewModel by hiltNavGraphViewModels(R.id.authorization_graph)
-    private var phoneNumberLength: Int? = null
-    private lateinit var maskChangedListener: MaskChangedListener
+    private val maskFormatter = MaskedFormatter("### ### ###")
+
+    override fun initialize() {
+        addMaskToThePhoneNumberEditText()
+        disableHelperText()
+    }
+
+
+    private fun addMaskToThePhoneNumberEditText() {
+        binding.etPhone.addTextChangedListener(MaskedWatcher(maskFormatter, binding.etPhone))
+    }
+
+    private fun disableHelperText() {
+    }
 
 
     override fun setupListeners() {
@@ -27,11 +46,21 @@ class SignUpFragment :
 
     private fun openPhoneNumberVerificationDialog() {
         binding.btnContinue.setOnClickListener {
-            findNavController().directionsSafeNavigation(
-                SignUpFragmentDirections.actionSignUpFragmentToPhoneVerificationDialogFragment(
-                    "${binding.etPhone.text?.trim()}"
+            if (binding.etPhone.text?.length != 11) {
+                binding.tlPhone.isErrorEnabled = true
+                binding.tlPhone.error = getString(R.string.phone_number_must_consist_of_9_digits)
+            } else
+
+                findNavController().directionsSafeNavigation(
+                    SignUpFragmentDirections.actionSignUpFragmentToPhoneVerificationDialogFragment(
+                        binding.tvCountryPhoneCode.text.toString() + maskFormatter.formatString(
+                            binding.etPhone.text?.toString().toString()
+                        )?.unMaskedString
+                    )
                 )
-            )
+            binding.etPhone.addTextChangedListenerAnonymously(doSomethingOnTextChanged = {
+                binding.tlPhone.isErrorEnabled = false
+            })
         }
 
     }
