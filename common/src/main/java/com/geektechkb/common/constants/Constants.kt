f7b271a04f02b9cbb98223c61aab68122263c1b8
Clar1fy <timplifier@gmail.com>
2022-08-06 02:17:48 +0600

    added unique handler id constant

--- common/src/main/java/com/geektechkb/common/constants/Constants.kt
+++ common/src/main/java/com/geektechkb/common/constants/Constants.kt
@@ -23,4 +23,6 @@ object Constants {
     const val FIREBASE_CLOUD_STORAGE_VOICE_MESSAGES_PATH = "voiceMessages/"
 
     const val INTENT_FILTER = "MESSENGER_EVENT"
+
+    const val UNIQUE_HANDLER_ID = "uniqueHandlerId"
 }
