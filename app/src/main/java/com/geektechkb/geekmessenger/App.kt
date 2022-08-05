c4698c520a26748e7f1bd07cdd77e22bccac5ecb
Clar1fy <timplifier@gmail.com>
2022-08-06 02:13:36 +0600

    initialized sendbird

--- app/src/main/java/com/geektechkb/geekmessenger/App.kt
+++ app/src/main/java/com/geektechkb/geekmessenger/App.kt
@@ -2,23 +2,25 @@ package com.geektechkb.geekmessenger
 
 import android.app.Application
 import android.util.Log
+import com.geektechkb.common.constants.Constants.UNIQUE_HANDLER_ID
 import com.geektechkb.feature_main.BuildConfig
 import com.google.firebase.messaging.FirebaseMessaging
+import com.sendbird.calls.DirectCall
 import com.sendbird.calls.SendBirdCall
+import com.sendbird.calls.handler.SendBirdCallListener
 import com.vanniktech.emoji.EmojiManager
 import com.vanniktech.emoji.google.GoogleEmojiProvider
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class App : Application() {
-    val emojiManager = EmojiManager.install(GoogleEmojiProvider())
+
 
     override fun onCreate() {
         super.onCreate()
-        SendBirdCall.init(
-            applicationContext,
-            BuildConfig.SENDBIRD_APP_ID
-        )
+        initSendBird()
+        installEmojiProvider()
+
         FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
             if (!task.isSuccessful) {
                 return@addOnCompleteListener
@@ -27,5 +29,23 @@ class App : Application() {
             val token = task.result
             Log.e("RUSS", "Token -> $token")
         }
+        SendBirdCall.addListener(UNIQUE_HANDLER_ID, object : SendBirdCallListener() {
+            override fun onRinging(call: DirectCall) {
+            }
+
+        })
     }
+
+    private fun installEmojiProvider() {
+        EmojiManager.install(GoogleEmojiProvider())
+    }
+
+    private fun initSendBird() {
+        SendBirdCall.init(
+            applicationContext,
+            BuildConfig.SENDBIRD_APP_ID
+        )
+
+    }
+
 }
