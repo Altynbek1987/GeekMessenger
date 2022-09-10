00658c0da7bd366a95cefa98c3104991816214a6
Clar1fy <timplifier@gmail.com>
2022-09-11 00:39:47 +0600

    better permission request,photo selection and handled no photo authentication

--- feature-auth/src/main/java/com/geektechkb/feature_auth/presentation/ui/fragments/auth/createProfile/CreateProfileFragment.kt
+++ feature-auth/src/main/java/com/geektechkb/feature_auth/presentation/ui/fragments/auth/createProfile/CreateProfileFragment.kt
@@ -2,19 +2,19 @@ package com.geektechkb.feature_auth.presentation.ui.fragments.auth.createProfile
 
 import android.Manifest
 import android.net.Uri
-import androidx.activity.result.contract.ActivityResultContracts
+import android.util.Log
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.geektechkb.core.base.BaseFragment
 import com.geektechkb.core.extensions.*
 import com.geektechkb.feature_auth.R
+import com.geektechkb.feature_auth.data.local.preferences.AuthorizePreferences
 import com.geektechkb.feature_auth.databinding.FragmentCreateProfileBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class CreateProfileFragment :
@@ -23,80 +23,93 @@ class CreateProfileFragment :
     override val viewModel by viewModels<CreateProfileViewModel>()
     private val args: CreateProfileFragmentArgs by navArgs()
     private var uri: Uri? = null
+    private val readExternalStoragePermissionLauncher =
+        createRequestPermissionLauncherToRequestSinglePermission(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            actionWhenPermissionHasBeenGranted = {
+                fileChooserContract.launch("image/*")
+            },
+            actionWhenPermissionHasBeenDenied = {
+                if (findNavController().currentDestination?.id != R.id.deniedPermissionsDialogFragment)
+                    findNavController().directionsSafeNavigation(
+                        CreateProfileFragmentDirections.actionCreateProfileFragmentToDeniedPermissionsDialogFragment(
+                            getString(com.geektechkb.core.R.string.geekMessenger_application_cant_function_without_needed_permissions_russian)
+                        )
+                    )
+            }
+        )
+    private val fileChooserContract = createFileChooserContractToGetImageUri { imageUri ->
+        binding.imProfile.setImage(imageUri.toString())
+        uri = imageUri
+        binding.tvPhotoSelection.text = "Изменить фото профиля"
+        binding.tvText.isVisible = false
+    }
+
+    @Inject
+    lateinit var authorizePreferences: AuthorizePreferences
 
     override fun setupListeners() {
-        val requestPermissionLauncher = registerForActivityResult(
-            ActivityResultContracts.RequestMultiplePermissions()
-        ) { isGranted ->
-            for (permission in isGranted) {
-                when {
-                    permission.value -> fileChooserContract.launch("image/*")
-                    !shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE) -> {
-                        permissionMessage()
-                    }
-                }
-            }
-        }
+        setProfileAvatar()
+        authenticateUser()
+    }
 
-        binding.apply {
-            binding.btnSignIn.setOnClickListener {
-                if (etName.text.isNullOrEmpty() || etName.text.isNullOrBlank()) {
-                    etName.error = "Это поле обязательно для заполнения"
-                } else {
-                    viewModel.isUserAuthenticated()
-                    lifecycleScope.launch {
-                        viewModel.authenticateUser(
-                            "был(а) недавно",
-                            args.phoneNumber,
-                            binding.etName.text.toString(),
-                            binding.etSurname.text.toString(),
-                            uri.toString(),
-                            generateRandomId()
-                        ) {
-                            viewModel.isUserAuthenticated()
-                            mainNavController(R.id.nav_host_fragment_container_auth).navigateSafely(
-                                R.id.action_profileFragment_to_mainFlowFragment
-                            )
-                        }
-                    }
-                }
-            }
-
-            binding.imProfile.setOnClickListener {
-                if (checkForMultiplePermissionsAndRequestThem(
-                        requestPermissionLauncher,
-                        arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
-                    )
-                ) {
+    private fun setProfileAvatar() {
+        binding.imProfile.setOnClickListener {
+            checkForPermissionStatusAndRequestIt(
+                readExternalStoragePermissionLauncher,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                actionWhenPermissionHasBeenGranted = {
                     fileChooserContract.launch("image/*")
+                })
+        }
+        binding.tvPhotoSelection.setOnClickListener {
+            checkForPermissionStatusAndRequestIt(
+                readExternalStoragePermissionLauncher,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                actionWhenPermissionHasBeenGranted = {
+                    fileChooserContract.launch("image/*")
+                })
+        }
+    }
+
+    private fun authenticateUser() {
+        binding.apply {
+            btnSignIn.setOnClickListener {
+                if (etName.text.isNullOrEmpty() || etName.text.isNullOrBlank()) {
+                    tilName.isErrorEnabled = true
+                    tilName.error = "Это поле обязательно для заполнения"
+                } else {
+                    viewModel.authenticateUser(
+                        "был(а) недавно",
+                        args.phoneNumber,
+                        etName.text.toString(),
+                        etSurname.text.toString(),
+                        uri.toString(),
+                        generateRandomId()
+                    )
                 }
-
-
             }
-
-        }
-        binding.ibBack.setOnClickListener {
-            findNavController().navigate(R.id.verifyAuthenticationFragment)
+            binding.etName.addTextChangedListenerAnonymously(doSomethingOnTextChanged = {
+                binding.tilName.error = null
+                binding.tilName.isErrorEnabled = false
+            })
         }
     }
 
-    private fun permissionMessage() {
-        if (findNavController().currentDestination?.id != R.id.deniedPermissionsDialogFragment) {
-            findNavController().navigate(
-                CreateProfileFragmentDirections.actionProfileFragmentToDeniedPermissionsDialogFragment(
-                    "Приложение GeekMessenger не может функционировать без разрешение на галерею устройства. Вы можете включить их в разределе Настройки"
-                )
+    override fun launchObservers() {
+        subscribeToAuthentication()
+    }
+
+    private fun subscribeToAuthentication() {
+        viewModel.authenticationState.spectateUiState(success = {
+            authorizePreferences.isAuthorized = true
+            mainNavController(R.id.nav_host_fragment_container_auth).navigateSafely(
+                R.id.action_profileFragment_to_mainFlowFragment
             )
-        }
+        }, error = {
+            Log.e("gaypopError", it)
+        }, gatherIfSucceed = {
+            it.assembleViewVisibility(binding.gCreateProfile, binding.cpiCreateProfile)
+        })
     }
-
-    private val fileChooserContract =
-        registerForActivityResult(ActivityResultContracts.GetContent()) { imageUri ->
-            if (imageUri != null) {
-                binding.imProfile.setImage(imageUri.toString())
-                uri = imageUri
-                binding.tvPhotoSelection.text = "Изменить фото профиля"
-                binding.tvText.isVisible = false
-            }
-        }
 }
