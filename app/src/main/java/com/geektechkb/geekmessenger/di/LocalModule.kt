29448bfe841d3f355ba0ff72f96bf370290e9836
Clar1fy <timplifier@gmail.com>
2022-09-17 20:41:49 +0600

    deleted everything related to room

--- app/src/main/java/com/geektechkb/geekmessenger/di/LocalModule.kt
+++ app/src/main/java/com/geektechkb/geekmessenger/di/LocalModule.kt
@@ -5,8 +5,6 @@ import android.content.SharedPreferences
 import com.geektechkb.core.data.local.preferences.UserPreferencesHelper
 import com.geektechkb.feature_auth.data.local.preferences.AuthorizePreferences
 import com.geektechkb.feature_auth.data.local.preferences.OnBoardPreferencesHelper
-import com.geektechkb.feature_main.data.local.db.AppDataBase
-import com.geektechkb.feature_main.data.local.db.RoomClient
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -37,14 +35,4 @@ object LocalModule {
     @Provides
     fun provideUserPreferences(preferences: SharedPreferences) =
         UserPreferencesHelper(preferences)
-
-    @Singleton
-    @Provides
-    fun provideAppDatabase(@ApplicationContext context: Context) =
-        RoomClient().provideCreateAppDataBase(context)
-
-    @Singleton
-    @Provides
-    fun provideUserDao(appDataBase: AppDataBase) =
-        RoomClient().provideUserDao(appDataBase)
 }
