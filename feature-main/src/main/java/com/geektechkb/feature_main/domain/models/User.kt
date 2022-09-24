ccc0a97efa8f2d91a64ba3b5e4a15404eefec4ee
Clar1fy <timplifier@gmail.com>
2022-09-24 23:42:55 +0600

    deleted highlightable thing

--- feature-main/src/main/java/com/geektechkb/feature_main/domain/models/User.kt
+++ feature-main/src/main/java/com/geektechkb/feature_main/domain/models/User.kt
@@ -1,22 +1,15 @@
 package com.geektechkb.feature_main.domain.models
 
 
-import com.algolia.instantsearch.core.highlighting.HighlightedString
-import com.algolia.instantsearch.highlighting.Highlightable
-import com.algolia.search.model.Attribute
 import com.geektechkb.core.base.BaseDiffModel
-import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.Serializable
 
-@kotlinx.serialization.Serializable
+@Serializable
 data class User(
     val name: String? = null,
     val lastName: String? = null,
     override val phoneNumber: String? = null,
-    val profileImage: String? = null,
+    var profileImage: String? = null,
     val lastSeen: String? = null,
     val isPhoneNumberHidden: Boolean? = null,
-    override val _highlightResult: JsonObject? = null,
-) : BaseDiffModel, Highlightable {
-    val highlightedName: HighlightedString?
-        get() = getHighlight(Attribute("name"))
-}
+) : BaseDiffModel
