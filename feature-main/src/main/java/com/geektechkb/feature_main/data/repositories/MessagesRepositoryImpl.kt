ccbfe7bdd139b3368871c4be9b43fad0c4e624d9
Clar1fy <timplifier@gmail.com>
2022-10-01 12:45:47 +0600

    also send image if message contains one

--- feature-main/src/main/java/com/geektechkb/feature_main/data/repositories/MessagesRepositoryImpl.kt
+++ feature-main/src/main/java/com/geektechkb/feature_main/data/repositories/MessagesRepositoryImpl.kt
@@ -1,24 +1,27 @@
 package com.geektechkb.feature_main.data.repositories
 
 import android.net.Uri
+import com.geektechkb.common.constants.Constants.FIREBASE_CLOUD_STORAGE_MESSAGE_IMAGES_PATH
 import com.geektechkb.common.constants.Constants.FIREBASE_CLOUD_STORAGE_VOICE_MESSAGES_PATH
 import com.geektechkb.common.constants.Constants.FIREBASE_FIRESTORE_MESSAGES_COLLECTION_PATH
 import com.geektechkb.common.constants.Constants.FIREBASE_FIRESTORE_TIME_MESSAGE_WAS_SENT
 import com.geektechkb.core.base.BaseRepository
+import com.geektechkb.core.extensions.generateRandomId
 import com.geektechkb.core.extensions.snapshotFlow
 import com.geektechkb.feature_main.domain.models.Message
 import com.geektechkb.feature_main.domain.repositories.MessagesRepository
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageException
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.tasks.await
+import java.io.FileNotFoundException
 import javax.inject.Inject
 
 class MessagesRepositoryImpl @Inject constructor(
     firestore: FirebaseFirestore,
     cloudStorage: FirebaseStorage,
-    private val firebaseAuth: FirebaseAuth,
 ) : BaseRepository(), MessagesRepository {
     private val messagesRef =
         firestore.collection(FIREBASE_FIRESTORE_MESSAGES_COLLECTION_PATH)
@@ -31,20 +34,57 @@ class MessagesRepositoryImpl @Inject constructor(
         id: String,
         receiverPhoneNumber: String,
         message: String,
+        image: String,
         timeMessageWasSent: String,
         messageId: String,
     ) {
-        messageMap["messageId"] = messageId
-        messageMap["messageKey"] = (id + receiverPhoneNumber)
-        messageMap["message"] = message
-        messageMap["senderPhoneNumber"] = id
-        messageMap["receiverPhoneNumber"] = receiverPhoneNumber
-        messageMap["timeMessageWasSent"] = timeMessageWasSent
-        addDocument(
-            messagesRef,
-            messageMap,
-            messageId
-        )
+        try {
+            messageMap["messageId"] = messageId
+            messageMap["messageKey"] = (id + receiverPhoneNumber)
+            messageMap["message"] = message
+            messageMap["mediaResource"] =
+                uploadUncompressedImageToCloudStorage(
+                    cloudStorageRef,
+                    Uri.parse(image),
+                    FIREBASE_CLOUD_STORAGE_MESSAGE_IMAGES_PATH, generateRandomId()
+                )
+            messageMap["senderPhoneNumber"] = id
+            messageMap["receiverPhoneNumber"] = receiverPhoneNumber
+            messageMap["timeMessageWasSent"] = timeMessageWasSent
+            addDocument(
+                messagesRef,
+                messageMap,
+                messageId
+            )
+        } catch (e: StorageException) {
+            messageMap["messageId"] = messageId
+            messageMap["messageKey"] = (id + receiverPhoneNumber)
+            messageMap["message"] = message
+            messageMap["mediaResource"] = image
+            messageMap["senderPhoneNumber"] = id
+            messageMap["receiverPhoneNumber"] = receiverPhoneNumber
+            messageMap["timeMessageWasSent"] = timeMessageWasSent
+            addDocument(
+                messagesRef,
+                messageMap,
+                messageId
+            )
+
+        } catch (e: FileNotFoundException) {
+            messageMap["messageId"] = messageId
+            messageMap["messageKey"] = (id + receiverPhoneNumber)
+            messageMap["message"] = message
+            messageMap["mediaResource"] = image
+            messageMap["senderPhoneNumber"] = id
+            messageMap["receiverPhoneNumber"] = receiverPhoneNumber
+            messageMap["timeMessageWasSent"] = timeMessageWasSent
+            addDocument(
+                messagesRef,
+                messageMap,
+                messageId
+            )
+        }
+
     }
 
     override suspend fun sendVoiceMessage(file: String, voiceFileName: String) {
