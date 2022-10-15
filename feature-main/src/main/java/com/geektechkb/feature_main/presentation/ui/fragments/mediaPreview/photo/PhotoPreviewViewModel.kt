e866a70da2a8c34cd458840acec5965fb0b95c37
Clar1fy <timplifier@gmail.com>
2022-10-16 00:34:04 +0600

    preview related logic and cleaned up the viewmodel due to its uselessness

--- feature-main/src/main/java/com/geektechkb/feature_main/presentation/ui/fragments/mediaPreview/photo/PhotoPreviewViewModel.kt
+++ feature-main/src/main/java/com/geektechkb/feature_main/presentation/ui/fragments/mediaPreview/photo/PhotoPreviewViewModel.kt
@@ -1,33 +1,10 @@
 package com.geektechkb.feature_main.presentation.ui.fragments.mediaPreview.photo
 
 import com.geektechkb.core.base.BaseViewModel
-import com.geektechkb.feature_main.domain.useCases.SendMessageUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class PhotoPreviewViewModel @Inject constructor(
-    private val sendMessageUseCase: SendMessageUseCase
 ) : BaseViewModel() {
-//    fun sendMessage(
-//        id: String,
-//        receiverPhoneNumber: String,
-//        message: String,
-//        image: String,
-//        mediaType: String?,
-//        timeMessageWasSent: String,
-//        messageId: String,
-//    ) {
-//        viewModelScope.launch {
-//            sendMessageUseCase(
-//                id,
-//                receiverPhoneNumber,
-//                message,
-//                image,
-//                mediaType,
-//                timeMessageWasSent,
-//                messageId
-//            )
-//        }
-//    }
 }
